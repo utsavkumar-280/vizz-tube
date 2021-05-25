6a9673883977418c6530c485f3b90a77c1551339
Utsav Kumar <utsavkumar280@gmail.com>
2021-05-25 14:22:47 +0530

    feat: added login component

--- src/Components/Auth/Login.jsx
+++ src/Components/Auth/Login.jsx
@@ -3,7 +3,9 @@ import "./styles.css";
 export const Login = () => {
 	return (
 		<div className="login-container">
-			<div className="login-main"></div>
+			<div className="login-main">
+				<h1>Hello Its me login</h1>
+			</div>
 		</div>
 	);
 };
