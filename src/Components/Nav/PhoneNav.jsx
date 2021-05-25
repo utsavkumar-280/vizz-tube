d7069cf1f2d815cd0d1be8f06c0a2f22fdabc13b
Utsav Kumar <utsavkumar280@gmail.com>
2021-05-25 14:24:06 +0530

    feat: added PhoneNav component

---
+++ src/Components/Nav/PhoneNav.jsx
@@ -0,0 +1,11 @@
+import "./phoneNav.css";
+
+export const PhoneNav = () => {
+	return (
+		<div className="phone-nav-container">
+			<div className="phone-nav-main">
+				<h2>Its me PhoneNav</h2>
+			</div>
+		</div>
+	);
+};
