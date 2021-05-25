9446822207c57934e1d817497ecb0ffa2bd73aef
Utsav Kumar <utsavkumar280@gmail.com>
2021-05-25 14:20:19 +0530

    feat: created footer component

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,42 @@
 import "./styles.css";
+import { Link } from "react-router-dom";
+import { FaYoutube } from "react-icons/fa";
 
 export const Footer = () => {
 	return (
 		<div className="footer-container">
-			<div className="footer-main"></div>
+			<div className="footer-main">
+				<div className="footer-left ">
+					<div className="footer-padding footer-para">Connect with me</div>
+					<div className="socialNav">
+						<a href="https://www.linkedin.com/in/utsav-kumar-a3a79b187/">
+							<i class="fab fa-linkedin-in"></i>
+						</a>
+						<a href="https://twitter.com/utsavkumar280">
+							<i class="fab fa-twitter"></i>
+						</a>
+						<a href="https://github.com/utsavkumar-280">
+							<i class="fab fa-github"></i>
+						</a>
+					</div>
+				</div>
+
+				<div className="footer-right">
+					<Link to="/" className="footer-logo">
+						<FaYoutube className="head-logo" />
+						vizztube
+					</Link>
+					<div className="connectMe">
+						<div className="footer-name-padding">BY</div>
+						<a
+							href="https://utsav-kumar.netlify.app/"
+							className=" footer-name-padding"
+						>
+							@UTSAV
+						</a>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
