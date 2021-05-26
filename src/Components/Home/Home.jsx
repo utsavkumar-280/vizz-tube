77192fd361820708a194ff838c490f5cbd9e8755
Utsav Kumar <utsavkumar280@gmail.com>
2021-05-26 14:46:20 +0530

    feat: added buttons in discover section

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,4 +1,5 @@
 import "./styles.css";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
 	return (
@@ -14,6 +15,9 @@ export const Home = () => {
 								of Photography
 								<br />
 							</p>
+							<Link to="/explore">
+								<button className="discover-button">Watch Now</button>
+							</Link>
 						</div>
 						<div className="discover-edit">
 							<p>
@@ -23,6 +27,9 @@ export const Home = () => {
 								<br />
 								right now
 							</p>
+							<Link to="/explore">
+								<button className="discover-button">Watch Now</button>
+							</Link>
 						</div>
 					</div>
 				</div>
