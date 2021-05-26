91ac54b734552a52918d90d8632b0c120c9d9338
Utsav Kumar <utsavkumar280@gmail.com>
2021-05-26 09:27:42 +0530

    feat: added discover section in Home page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,7 +4,37 @@ export const Home = () => {
 	return (
 		<div className="home-container">
 			<div className="home-main">
-				<h1>Hello, Its me Home</h1>
+				<div className="discover-container">
+					<div className="discover-head">Discover</div>
+					<div className="discover-main">
+						<div className="discover-photo">
+							<p>
+								The What, Why and How
+								<br />
+								of Photography
+								<br />
+							</p>
+						</div>
+						<div className="discover-edit">
+							<p>
+								Video Editing
+								<br />
+								Tips you need to know
+								<br />
+								right now
+							</p>
+						</div>
+					</div>
+				</div>
+				<div className="popular-container">
+					<div className="popular-head">Popular</div>
+					<div className="popular-main">
+						<div className="popular"></div>
+						<div className="popular"></div>
+						<div className="popular"></div>
+						<div className="popular"></div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
