7e521cdba48e8e0dd0b1160cf99b8813e96dcbda
Utsav Kumar <utsavkumar280@gmail.com>
2021-06-01 12:44:40 +0530

    refact:

--- src/Components/VideoPages/NotRelated/NotRelated.jsx
+++ src/Components/VideoPages/NotRelated/NotRelated.jsx
@@ -5,7 +5,7 @@ export const NotRelated = () => {
 		<div className="not-related-container">
 			<div className="not-related-main">
 				<iframe
-					src="https://www.youtube.com/embed/6vKucgAeF_Q"
+					src="https://www.youtube-nocookie.com/embed/DK11J1KhPRs"
 					title="YouTube video player"
 					frameborder="0"
 					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen;"
