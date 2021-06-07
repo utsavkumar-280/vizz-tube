7f485c227fdbab2b08f58733027f811b4909c7c1
Utsav Kumar <utsavkumar280@gmail.com>
2021-06-07 23:43:57 +0530

    feat: added video cards and their styling

--- src/Components/VideoPages/Explore/Explore.jsx
+++ src/Components/VideoPages/Explore/Explore.jsx
@@ -1,10 +1,41 @@
 import "./explore.css";
+import { VideoCard } from "../../VideoCard/VideoCard";
+import { useLocation, NavLink } from "react-router-dom";
+import { categories } from "../../../utils/data";
+import { useAppDataContext } from "../../../Context";
 
 export const Explore = () => {
+	const { state } = useAppDataContext();
+	const search = new URLSearchParams(useLocation().search);
+	const searchedCategory = search.get("cat") ? search.get("cat") : "All Videos";
 	return (
 		<div className="explore-container">
 			<div className="explore-main">
-				<h1>Hello its me explore</h1>
+				<nav className="category-toggle">
+					{categories.map((category) => {
+						return (
+							<NavLink
+								to={`?cat=${category}`}
+								className={`category-cta ${
+									searchedCategory === category ? "category-cta-active" : ""
+								}`}
+							>
+								{category}
+							</NavLink>
+						);
+					})}
+				</nav>
+				<section className="video-card-container">
+					{searchedCategory === "All Videos"
+						? state.videos.map((video) => {
+								return <VideoCard video={video} key={video._id} />;
+						  })
+						: state.videos
+								.filter((video) => video.category === searchedCategory)
+								.map((video) => {
+									return <VideoCard video={video} key={video._id} />;
+								})}
+				</section>
 			</div>
 		</div>
 	);
