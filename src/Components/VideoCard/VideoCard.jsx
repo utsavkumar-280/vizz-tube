f012e628d8ddd089db821b71bbe27b4d23a79291
Utsav Kumar <utsavkumar280@gmail.com>
2021-06-07 23:40:34 +0530

    feat: added video card component

---
+++ src/Components/VideoCard/VideoCard.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+import "./videoCard.css";
+
+export const VideoCard = ({ video }) => {
+	return (
+		<div className="card-container">
+			<Link to="/login" className="card-thumbnail-container">
+				<img
+					src={video.thumbnail}
+					alt="thumbnail-img"
+					className="card-thumbnail"
+				/>
+			</Link>
+
+			<article className="card-info">
+				<p className="card-cat">#{video.category} </p>
+				<Link to="/login" className="card-title">
+					<h2>{video.title}</h2>
+				</Link>
+				<h3 className="card-author">{video.author}</h3>
+			</article>
+			<img src={video.authorImg} alt="avatar-img" className="card-avatar" />
+		</div>
+	);
+};
