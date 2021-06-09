758b0706c9529e80721cbf9b1d385df490d6d428
Utsav Kumar <utsavkumar280@gmail.com>
2021-06-09 23:46:01 +0530

    feat: added toast

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { ChakraProvider } from "@chakra-ui/react";
 import { AppDataProvider } from "./Context";
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
 	<StrictMode>
 		<Router>
-			<AppDataProvider>
-				<App />
-			</AppDataProvider>
+			<ChakraProvider>
+				<AppDataProvider>
+					<App />
+				</AppDataProvider>
+			</ChakraProvider>
 		</Router>
 	</StrictMode>,
 	document.getElementById("root")
