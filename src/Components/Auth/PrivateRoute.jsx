d2b540e4c21f05ab9fb5ada36410fa7c4bdb51fa
Utsav Kumar <utsavkumar280@gmail.com>
2021-06-29 22:31:03 +0530

    chore:

--- src/Components/Auth/PrivateRoute.jsx
+++ src/Components/Auth/PrivateRoute.jsx
@@ -1,7 +1,7 @@
 import { Navigate, Route } from "react-router-dom";
 
 export const PrivateRoute = ({ path, ...props }) => {
-	const isLoggedIn = true;
+	const isLoggedIn = false;
 
 	return isLoggedIn ? (
 		<Route {...props} path={path} />
