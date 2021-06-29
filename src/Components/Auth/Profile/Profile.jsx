18eacf2eeadee81692c3285b193c34a03f5c2cc5
Utsav Kumar <utsavkumar280@gmail.com>
2021-06-29 22:12:48 +0530

    feat: created profile page

---
+++ src/Components/Auth/Profile/Profile.jsx
@@ -0,0 +1,9 @@
+export const Profile = () => {
+	return (
+		<div className="profile-container">
+			<div className="profile-main">
+				<h1>Hello Im Profile</h1>
+			</div>
+		</div>
+	);
+};
--- src/Components/Auth/index.js
+++ src/Components/Auth/index.js
@@ -1,3 +1,4 @@
+export { Profile } from "./Profile";
 export { Login } from "./Login";
 export { ForgotPassword } from "./ForgotPassword";
 export { Signup } from "./Signup";
