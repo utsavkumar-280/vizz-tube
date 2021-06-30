71b89f9cfc71ad2080ba1d2e3e8d464a6ac076d9
Utsav Kumar <utsavkumar280@gmail.com>
2021-07-01 01:10:39 +0530

    feat: added profile page

--- src/Components/Auth/Profile/Profile.jsx
+++ src/Components/Auth/Profile/Profile.jsx
@@ -1,8 +1,68 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useAuth } from "../../../Context";
+import { VIZZ_API } from "../../../utils";
+import { FaUserCircle } from "react-icons/fa";
+import "./styles.css";
+
 export const Profile = () => {
+	const {
+		state: { userDetails, token },
+		dispatch,
+		logout,
+	} = useAuth();
+
+	useEffect(() => {
+		if (!userDetails) {
+			(async () => {
+				try {
+					const {
+						data: { response },
+						status,
+					} = await axios({
+						url: `${VIZZ_API}/users/info`,
+						method: "GET",
+						headers: {
+							Authorization: `Bearer ${token}`,
+						},
+					});
+					console.log({ response });
+
+					if (status === 200) {
+						dispatch({
+							type: "SET_USER_DETAILS",
+							payload: { userDetails: response },
+						});
+					}
+				} catch (error) {
+					console.log(error);
+				}
+			})();
+		}
+	}, [token]);
 	return (
 		<div className="profile-container">
 			<div className="profile-main">
-				<h1>Hello Im Profile</h1>
+				<div className="profile-card">
+					<FaUserCircle className="profile-logo" />
+					<div>
+						<p className="profile-text">
+							Firstname:{" "}
+							<span className="primary-color">{userDetails?.firstname}</span>
+						</p>
+						<p className="profile-text">
+							Lastname:{" "}
+							<span className="primary-color">{userDetails?.lastname}</span>
+						</p>
+						<p className="profile-text">
+							Email: <span className="primary-color">{userDetails?.email}</span>
+						</p>
+					</div>
+
+					<button onClick={() => logout()} className="form-submit-cta">
+						Logout
+					</button>
+				</div>
 			</div>
 		</div>
 	);
