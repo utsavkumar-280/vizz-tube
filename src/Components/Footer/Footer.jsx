c7374308b977f8eafcec3fcb1e3d1d62204ffa0d
Utsav Kumar <66728108+utsavkumar-280@users.noreply.github.com>
2022-01-06 13:15:09 +0530

    feat: changed footer structure

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,31 +6,7 @@ export const Footer = () => {
 	return (
 		<div className="footer-container">
 			<div className="footer-main">
-				<div className="footer-left ">
-					<div className="footer-padding footer-para">Connect with me</div>
-					<div className="socialNav">
-						<a
-							href="https://www.linkedin.com/in/utsav-kumar-a3a79b187/"
-							aria-label="Linkedin-profile"
-						>
-							<i className="fab fa-linkedin-in"></i>
-						</a>
-						<a
-							href="https://twitter.com/utsavkumar280"
-							aria-label="Twitter-handle"
-						>
-							<i className="fab fa-twitter"></i>
-						</a>
-						<a
-							href="https://github.com/utsavkumar-280"
-							aria-label="Github-profile"
-						>
-							<i className="fab fa-github"></i>
-						</a>
-					</div>
-				</div>
-
-				<div className="footer-right">
+				<div className="footer-content">
 					<Link to="/" className="footer-logo">
 						<FaYoutube className="head-logo" />
 						vizztube
